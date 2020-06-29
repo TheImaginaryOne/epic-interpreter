c6f25d7d197bc121e906e7425a32d05ca79e5a45
TheImaginaryOne <johnathanleung@hotmail.co.nz>
2020-06-29 12:26:50 +1200

    Add all string representations of tokens for reporting purposes

--- src/compiler/utils.rs
+++ src/compiler/utils.rs
@@ -2,10 +2,29 @@ use crate::compiler::lexer::Token;
 
 pub fn token_to_string(token: Token) -> String {
     match token {
+        Token::String => "<string>",
+        Token::Integer => "<integer>",
+        Token::Identifier => "<identifier>",
+
+        Token::Plus => "+",
+        Token::Minus => "-",
+        Token::Star => "*",
+        Token::Slash => "/",
+        Token::LParen => "(",
         Token::RParen => ")",
+        Token::LBrace => "{",
+        Token::RBrace => "}",
         Token::Semicolon => ";",
+
+        Token::Equal => "=",
+        Token::DoubleEqual => "==",
+        Token::Less => "<",
+        Token::Greater => ">",
+        Token::Let => "let",
+        Token::If => "if",
+        Token::Else => "else",
         Token::Eof => "end of file",
-        _ => "",
+        _ => " "
     }
     .to_string()
 }
